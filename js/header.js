45504c54ec0147bdf1f9b46d26a1b7f50cfa504c
Requals <86450119+Requals@users.noreply.github.com>
2023-02-12 13:09:26 -0500

    test commit

---
+++ js/header.js
@@ -0,0 +1,34 @@
+const availableThemes = ["dark", "light", "twentysecond", "custom"];
+var siteData;
+
+function validateData(data) {
+	if (availableThemes.includes(data.theme) === false) {
+		throw new Error("Theme does not exist!");
+	}
+	for (games in data.games) {
+		if (typeof data.games[games] !== "boolean") {
+			throw new Error("Game configuration must be a boolean!");
+		}
+	}
+	return false;
+}
+
+function handleData(data) {
+	validateData(data);
+	console.log(data);
+	themeData = document.createElement("link");
+	themeData.rel = "stylesheet";
+	if (document.body.classList.contains("index")) {
+		themeData.href = `css/${data.theme}.css`;
+	} else {
+		themeData.href = `../css/${data.theme}.css`;
+	}
+	document.head.appendChild(themeData);
+}
+
+fetch("../neuralekzz.json")
+	.then((response) => response.json())
+	.then((data) => {
+		siteData = data;
+		handleData(data);
+	});
