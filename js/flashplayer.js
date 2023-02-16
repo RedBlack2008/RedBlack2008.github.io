1256030bcc2983e01c2cf2d67b7385896c8821fd
Requals <86450119+Requals@users.noreply.github.com>
2023-02-15 19:54:51 -0500

    downgrade ruffle to 2022 12 22 like 3kh0.github.io

--- js/flashplayer.js
+++ js/flashplayer.js
@@ -2,23 +2,23 @@ let game;
 function setTitle(gamedata, gametype) {
 	const flashgames = gamedata.flash;
 	gametitle = flashgames.find((gamer) => gamer[0] === gametype)[1];
-
 	let title = document.querySelector("title");
 	title.innerHTML = `Neuralekzz | ${gametitle}`;
 }
 if (new URLSearchParams(window.location.search).has("game")) {
 	const container = document.querySelector(".container");
-	container.innerHTML = "";
 	urlparams = new URLSearchParams(window.location.search);
 	game = urlparams.get("game");
 	window.RufflePlayer = window.RufflePlayer || {};
 	window.addEventListener("load", (event) => {
 		const ruffle = window.RufflePlayer.newest();
+		const player = ruffle.createPlayer();
 		window.RufflePlayer.config = window.RufflePlayer.config || {};
 		window.RufflePlayer.config.letterbox = "on";
 		window.RufflePlayer.config.autoplay = "auto";
 		window.RufflePlayer.config.publicPath = "files";
-		const player = ruffle.createPlayer();
+		player.style.width = "100%";
+		player.style.height = "100%";
 		container.appendChild(player);
 		fetch(`https://raw.githubusercontent.com/neuralekzz/neuralekzz-assets/main/flash/${game}.swf`).then(player.load(`https://raw.githubusercontent.com/neuralekzz/neuralekzz-assets/main/flash/${game}.swf`));
 	});
